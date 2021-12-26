33b2982f20a25831eca44d78c81a09cb3f48c12b
wj-Mcat <1435130236@qq.com>
2021-12-26 23:22:33 +0800

    add yaml merger unit testing

--- tests/tools/test_yaml_merger.py
+++ tests/tools/test_yaml_merger.py
@@ -10,8 +10,10 @@ from rasa.shared.nlu.training_data.formats.rasa_yaml import (   # type: ignore
 )
 from rasa.shared.nlu.training_data.training_data import TrainingData    # type: ignore
 from rasa.shared.core.training_data.story_reader.yaml_story_reader import YAMLStoryReader
+from rasa.shared.core.domain import Domain
 
 from src.config import test_dir
+from src.utils import find_yaml_files
 
 
 def test_simple_merger():
@@ -36,6 +38,15 @@ def test_simple_merger():
     writer = RasaYAMLWriter()
     writer.dump(output_file, training_data)
 
+def test_mixin():
+    reader = RasaYAMLReader()
+    file = os.path.join(test_dir, 'data/nlu/mix.yaml')
+    
+    training_data: TrainingData = reader.read(file)
+    assert len(training_data.responses) == 1
+    assert len(training_data.intents) == 1
+    
+
 
 def test_story_merger():
     """test story merger code"""
@@ -45,3 +56,19 @@ def test_story_merger():
         os.path.join(test_dir, 'data/stories/story.yml'), skip_validation=True
     )
     assert len(training_data) == 2
+
+
+def test_file_finder():
+    files = find_yaml_files(test_dir)
+    assert len(files) == 3
+
+
+def test_domain_reader():
+    domain_file = os.path.join(test_dir, 'data/domains/domain.yaml')
+    reader: Domain = Domain.load(domain_file)
+    assert len(reader.intents) == 7
+
+    training_data: TrainingData = RasaYAMLReader().read(domain_file)
+    assert len(training_data.intents) == 2
+
+    
